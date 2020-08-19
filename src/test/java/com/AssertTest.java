ef456a7c40812c113df3407484482890770f10e9
Heitor Bernardes Quaglia <heitor.bq@hotmail.com>
2020-08-19 11:51:11 -0300

    AssertTest

---
+++ src/test/java/com/AssertTest.java
@@ -0,0 +1,46 @@
+package com;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssertTest {
+
+    @Test
+    public void testAssertArrayEquals() {
+        byte[] esperado = "teste".getBytes();
+        byte[] atual = "teste".getBytes();
+
+        Assert.assertArrayEquals(esperado, atual);
+    }
+
+    @Test
+    public void testAssertEquals() {
+        Assert.assertEquals("text", "text");
+    }
+
+    @Test
+    public void testAssertFalse() {
+        Assert.assertFalse(false);
+    }
+
+    @Test
+    public void testAssertNotNull() {
+        Assert.assertNotNull(new Object());
+    }
+
+    @Test
+    public void testAssertNotSame() {
+        Assert.assertNotSame(new Object(), new Object());
+    }
+
+    @Test
+    public void testAssertNull() {
+        Assert.assertNull(null);
+    }
+
+    @Test
+    public void testAssertSame() {
+        Integer number = Integer.parseInt("1");
+        Assert.assertSame(number, number);
+    }
+}
