c438f2a50e0fe9b9a3e3d83674c828fe7c0d0b42
Heitor Bernardes Quaglia <heitor.bq@hotmail.com>
2020-08-19 12:49:40 -0300

    Rule Test

---
+++ src/test/java/com/teste/RuleTest.java
@@ -0,0 +1,22 @@
+package com.teste;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+
+public class RuleTest {
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
+
+    @Test
+    public void shouldCreateNewFileInTemporaryFolder() throws IOException {
+        File created = tmpFolder.newFile("file.txt");
+
+        Assert.assertTrue(created.isFile());
+        Assert.assertEquals(tmpFolder.getRoot(),created.getParentFile());
+    }
+}
